e4e2c5b45d8901e8e26b3f960593545c7f51c79b
Mayank <vijaymaynk2003@gmail.com>
2026-05-28 22:31:31 +0530

    feat: implement message broadcasting event handlers for socket connection

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -24,15 +24,21 @@ app.get("/", (req, res) => {
 io.on("connection", (socket) => {
   console.log("User connected:", socket.id);
 
+  // listen this event from client
   socket.on("send_message", (data) => {
     console.log(data);
 
+    // emit this event to all clients
     io.emit("receive_message", data);
   });
+
+  
 
   socket.on("disconnect", () => {
     console.log("User disconnected");
   });
+
+
 });
 
 server.listen(PORT, () => {
